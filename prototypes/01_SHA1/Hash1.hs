a89b385f2c3d0c822a2adbc027c204b4c2be4232
Greg Burri <greg.burri@gmail.com>
2009-04-04 14:44:51 +0200

    ADD some tests, not finished yet

---
+++ prototypes/01_SHA1/Hash1.hs
@@ -0,0 +1,20 @@
+import Data.Digest.Pure.SHA (Digest(..), sha1, bytestringDigest, showDigest)
+import qualified Data.ByteString.Lazy as L
+import System.Environment   
+
+main = do
+   args <- getArgs
+   hashes <- hashAFile 0 $ head args 
+   print hashes
+
+-- Offset + SHA1.
+data HashPart = HashPart Int Digest   
+instance Show HashPart where
+   show (HashPart offset digest) = show offset ++ " - " ++ showDigest digest
+
+-- Take a chunk size (in Byte) and a file path.
+-- Return a list of hashes corresponding to each chunk of data.
+hashAFile :: Int -> FilePath -> IO [HashPart]
+hashAFile size path = do
+   content <- L.readFile path
+   return $ [HashPart 0 (sha1 content)]
