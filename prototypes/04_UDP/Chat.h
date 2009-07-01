e31d985f682a8677c70df1098dbaf1dbe2412c54
Greg Burri <greg.burri@gmail.com>
2009-07-02 00:04:50 +0200

    ADD Support for UDP multicast for the fourth prototype :).

--- prototypes/04_UDP/Chat.h
+++ prototypes/04_UDP/Chat.h
@@ -7,9 +7,10 @@
 #include <QtNetwork/QUdpSocket>
 
 /**
-  * An instance of this class will listen for UDP datagram on a certain port.
+  * An instance of this class will listen for UDP datagram on a certain port and IP (class D).
+  * see : http://tldp.org/HOWTO/Multicast-HOWTO-2.html#ss2.1
   * When a message is received it will print it.
-  * Broadcast nessage can be sended eith 'sendMessage'.
+  * Multicast nessages can be sended with 'sendMessage'.
   */
 class Chat : public QObject
 {
@@ -29,7 +30,10 @@ private slots:
     
 private:
     QUdpSocket* socket;
-    static const int port = 34326;
+    
+    static const u_char TTL;
+    static const int port;
+    static QString multicastIP;
 };
 
 #endif // CHAT_H
