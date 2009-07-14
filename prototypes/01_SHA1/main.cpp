f97f62a0b1f1da07cef2283b4ed74bc2e18d94af
Greg Burri <greg.burri@gmail.com>
2009-07-15 00:40:31 +0200

    ADD Little diagram for the Core<->GUI protocol. MOD Little cleaning in 01_SHA1. ADD The events message to the GUI proto file.

--- prototypes/01_SHA1/main.cpp
+++ prototypes/01_SHA1/main.cpp
@@ -4,35 +4,45 @@
 #include <QtCore/QDebug>
 #include <QtCore/QList>
 
-#define BUFFER_SIZE 65536 // Buffer used when reading a file.
-#define CHUNK_SIZE 2097152 // 2 MB
+static const int BUFFER_SIZE = 65536; // (64kB) Buffer used when reading a file.
+static const int CHUNK_SIZE = 2097152; // (2 MB)
+
+class FileNotFoundException : public std::exception {};
 
 /**
   * Compute the SHA1 hash for the given file.
+  * @param filename The filename (with or without prefixed by a path). If the file
+  * is not found the exeption 'FileNotFoundException' is thrown.
+  * @param bufferSize The size of the buffer used to give the data to 'QCryptographicHash'.
+  * @return The sha1 hash.
   */
 QByteArray computeSHA1(const QString& filename, qint32 bufferSize)
+      throw (FileNotFoundException)
 {
    QCryptographicHash crypto(QCryptographicHash::Sha1);
       
    QFile file(filename);
    if (!file.open(QIODevice::ReadOnly))
-      return QByteArray();
+      throw FileNotFoundException();
 
    char buffer[bufferSize];
    qint64 bytesRead = 0;
    while ((bytesRead = file.read(buffer, bufferSize)) > 0)
-   {
       crypto.addData(buffer, bytesRead);
-   }
       
    return crypto.result();
 }
 
 /**
   * Compute some SHA1 hash for each chunk of the given file.
-  * 'chunkSize' must be a divisor of 'bufferSizer'.
+  * @param filename The filename (with or without prefixed by a path). If the file
+  * is not found the exeption 'FileNotFoundException' is thrown.
+  * @param chunkSize The size of each chunk. It must be a divisor of 'bufferSizer'.
+  * @param bufferSize The size of the buffer used to give the data to 'QCryptographicHash'.
+  * @return A list of chunk hashes.
   */
 QList<QByteArray> computeMultiSHA1(const QString& filename, qint32 chunkSize, qint32 bufferSize)
+      throw (FileNotFoundException)
 {
    QList<QByteArray> result;
    
@@ -40,7 +50,7 @@ QList<QByteArray> computeMultiSHA1(const QString& filename, qint32 chunkSize, qi
       
    QFile file(filename);
    if (!file.open(QIODevice::ReadOnly))
-      return QList<QByteArray>();
+      throw FileNotFoundException();
 
    char buffer[bufferSize];
    bool endOfFile = false;
@@ -65,6 +75,15 @@ QList<QByteArray> computeMultiSHA1(const QString& filename, qint32 chunkSize, qi
    return result;
 }
 
+/**
+  * Entry point.
+  * @param argc Must be equal to 3.
+  * @param argv Must contain :
+  *  1) the executable name.
+  *  2) "all" to compute the sha1 of the whole file or "chunk" for compute sha1 of each chunk.
+  *  3) The filename.
+  * @return 0 if all done fine else > 0
+  */
 int main(int argc, char *argv[])
 {
    QTextStream out(stdout);
