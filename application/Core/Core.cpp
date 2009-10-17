ccaa8f43f74b5c87ab18649ea1e6cfd43e71441b
Maximilien Cuony (The_glu) <theglu@theglu.org>
2009-10-17 12:04:43 +0200

    * Testing

--- application/Core/Core.cpp
+++ application/Core/Core.cpp
@@ -21,5 +21,8 @@ using namespace Core;
 
    this->logger->log("Ready to serve", LogManager::EndUser);
 
-
+ /*TESTING*/
+   NetworkListener::ISearch* s = this->networkListener->search();
+   s->search("coucou");
+   s->search("coucou2");
 }
