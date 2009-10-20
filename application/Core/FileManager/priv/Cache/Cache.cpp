b42b0b9058b974d7ae65e18452be436de712e40d
Greg Burri <greg.burri@gmail.com>
2009-10-20 21:23:35 +0200

    * Forget to add two files...

---
+++ application/Core/FileManager/priv/Cache/Cache.cpp
@@ -0,0 +1,86 @@
+#include <priv/Cache/Cache.h>
+using namespace FileManager;
+
+#include <QDir>
+
+#include <priv/Cache/SharedDirectory.h>
+#include <priv/FileManager.h>
+
+Cache::Cache(FileManager* fileManager, FileUpdater* fileUpdater)
+   : fileManager(fileManager), fileUpdater(fileUpdater)
+{
+}
+
+QStringList Cache::getSharedDirs(SharedDirectory::Rights rights)
+{
+   QStringList list;
+
+   for (QListIterator<SharedDirectory*> i(this->sharedDirs); i.hasNext();)
+   {
+      SharedDirectory* dir = i.next();
+      if (dir->getRights() == rights)
+         list << dir->getPath();
+   }
+   return list;
+}
+
+void Cache::setSharedDirs(const QStringList& dirs, SharedDirectory::Rights rights)
+{
+   // Filter the actual shared directories by looking theirs rights.
+   QList<SharedDirectory*> sharedDirs;
+   for(QListIterator<SharedDirectory*> i(this->sharedDirs); i.hasNext();)
+   {
+      SharedDirectory* dir = i.next();
+      if (dir->getRights() == rights)
+         sharedDirs << dir;
+   }
+
+   QStringList newDirs;
+   QMutableListIterator<SharedDirectory*> j(sharedDirs);
+
+   // O(n^2).
+   for(QListIterator<QString> i(dirs); i.hasNext();)
+   {
+      QString dir =  QDir::cleanPath(i.next());
+      j.toFront();
+      while(j.hasNext())
+      {
+         if (j.next()->getPath() == dir)
+         {
+            j.remove();
+            goto next;
+         }
+      }
+      newDirs << dir;
+      next:;
+   }
+
+   // Remove shared directories.
+   for (j.toFront(); j.hasNext();)
+   {
+      SharedDirectory* dir = j.next();
+      LOG_DEBUG("Remove a shared directory : " + dir->getPath());
+      this->fileUpdater->rmRoot(dir);
+      this->sharedDirs.removeOne(dir);
+   }
+
+   // Create new shared directories.
+   for (QListIterator<QString> i(newDirs); i.hasNext();)
+   {
+      QString path = i.next();
+      SharedDirectory* dir = new SharedDirectory(this, path);
+      LOG_DEBUG("Add a shared directory : " + dir->getPath());
+      this->fileUpdater->addRoot(dir);
+      this->sharedDirs << dir;
+   }
+}
+
+void Cache::onEntryAdded(Entry* entry)
+{
+   emit entryAdded(entry);
+}
+
+void Cache::onEntryRemoved(Entry* entry)
+{
+   emit entryRemoved(entry);
+}
---
+++ application/Core/FileManager/priv/Cache/Cache.h
@@ -0,0 +1,40 @@
+#ifndef FILEMANAGER_CACHE_H
+#define FILEMANAGER_CACHE_H
+
+#include <QObject>
+#include <QList>
+#include <QStringList>
+
+#include <priv/Cache/SharedDirectory.h>
+
+namespace FileManager
+{
+   class SharedDirectory;
+   class Entry;
+   class FileUpdater;
+   class FileManager;
+
+   class Cache : public QObject
+   {
+      Q_OBJECT
+   public:
+      Cache(FileManager* fileManager, FileUpdater* fileUpdater);
+
+      QStringList getSharedDirs(SharedDirectory::Rights rights);
+      void setSharedDirs(const QStringList& dirs, SharedDirectory::Rights rights);
+
+      void onEntryAdded(Entry* entry);
+      void onEntryRemoved(Entry* entry);
+
+   signals:
+      void entryAdded(Entry* entry);
+      void entryRemoved(Entry* entry);
+
+   private:
+      QList<SharedDirectory*> sharedDirs;
+
+      FileManager* fileManager;
+      FileUpdater* fileUpdater;
+   };
+}
+#endif
