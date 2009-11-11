80747aeb74c0c5790a81ab8f10618a572b635a5d
Greg Burri <greg.burri@gmail.com>
2009-11-11 11:39:14 +0100

    FM : Documentation.

--- application/Core/FileManager/priv/FileUpdater/WaitCondition.h
+++ application/Core/FileManager/priv/FileUpdater/WaitCondition.h
@@ -5,22 +5,38 @@ namespace FM
 {
    /**
      * A wait condition used instead of the Qt implementation (QWaitCondidition) for
-     * only one reason : to be able to use it natively with DirWatcher to wait
+     * only one reason : to be able to use it natively with DirWatcher to wait.
      */
    class WaitCondition
    {
    public:
+      /**
+        * Build a new wait condition which depends from the current platform.
+        */
       static WaitCondition* getNewWaitCondition();
 
       virtual ~WaitCondition() {}
 
+      /**
+        * Set the 'WaitCondition' in released state. 
+        *  - The next call to wait will not block
+        *  - If there is already a wait thread then it will be imediately released.
+        * Non-blocking call.
+        */
       virtual void release() = 0;
 
       /**
-        * Wait for a release. If a release has previously be asked then he will
-        * not be blocked.
+        * Wait for a release.
+        * If a release has previously be asked then the first call will not block.
+        * /!\ It's not the same behaviour than QWaitCondition!
         */
       virtual void wait() = 0;
+         
+      /**
+        * Return a pointer to a native structure.
+        * For example, on Windows it will return a HANDLE on the event.
+        * It can then be used with primitive like 'WaitForMultipleObjects'.
+        */
       virtual void* getHandle() = 0;
    };
 }
