939c8ec8c5e05afcfcaf3e127a377279a01f7339
olivier morel <omorel@gmail.com>
2009-11-29 00:12:37 +0100

    fix bug in LogManager and add Time information

--- application/Common/LogManager/priv/Logger.cpp
+++ application/Common/LogManager/priv/Logger.cpp
@@ -70,8 +70,9 @@ void Logger::log(const QString& message, Severity severity)
 {
    QMutexLocker lock(&Logger::mutex);
    QString threadName = QThread::currentThread()->objectName();
-   const QString& formatedMessage = QString("%1: [%2] (%3) %4: %5").arg(
+   const QString& formatedMessage = QString("%1 %2: [%3] (%4) %5: %6").arg(
       QDate::currentDate().toString("dd-MM-yyyy"),
+      QTime::currentTime().toString("HH:mm:ss"),
       Entry::SeverityToStr(severity),
       threadName.isEmpty() ? QString::number((quint32)QThread::currentThreadId()) : threadName,
       this->name,
@@ -88,19 +89,19 @@ void Logger::log(const QString& message, Severity severity)
 
 void Logger::log(const ILoggable& object, Severity severity)
 {
-
     QMutexLocker lock(&Logger::mutex);
     QString threadName = QThread::currentThread()->objectName();
-    const QString& formatedMessage = QString("%1: [%2] (%3) %4: %5").arg(
-            QDate::currentDate().toString("dd-MM-yyyy"),
-            Entry::SeverityToStr(severity),
-            threadName.isEmpty() ? QString::number((quint32)QThread::currentThreadId()) : threadName,
-            this->name,
-            object.toStringLog()
+    const QString& formatedMessage = QString("%1 %2: [%3] (%4) %5: %6").arg(
+       QDate::currentDate().toString("dd-MM-yyyy"),
+       QTime::currentTime().toString("HH:mm:ss"),
+       Entry::SeverityToStr(severity),
+       threadName.isEmpty() ? QString::number((quint32)QThread::currentThreadId()) : threadName,
+       this->name,
+       object.toStringLog()
     );
 
     if (Logger::out)
-        (*Logger::out) << formatedMessage << endl;
+       (*Logger::out) << formatedMessage << endl;
 
 #ifdef DEBG
     qDebug().nospace() << formatedMessage;
