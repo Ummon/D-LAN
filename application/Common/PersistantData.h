c4f9363f86cb352ccfed60d7b8f96aef64564324
Greg Burri <greg.burri@gmail.com>
2010-04-24 01:44:33 +0200

    Move doc from h to cpp.

--- application/Common/PersistantData.h
+++ application/Common/PersistantData.h
@@ -13,34 +13,13 @@ namespace Common
 {
    class UnknownValueException : public exception {};
 
-   /**
-     * Some little functions to persist data and retrieve it.
-     * The data are persisted in the user directory.
-     * The data are described by a Protocol Buffer message.
-     * Theses functions can be used for the application settings.
-     */
    class PersistantData
    {
       static const QString TEMP_SUFFIX_TERM;
    public:
 
-      /**
-        * Define a value associated to a name.
-        * You may refer to the name policy of the platform. Try to avoir special characters or space.
-        * You can use an extension in the name like "settings.conf".
-        */
       static void setValue(const QString& name, const google::protobuf::Message& data);
-
-      /**
-        * Retrieve the data associated to a given name.
-        * @exception UnknownValueException Throwed if the value doesn't exist
-        */
       static void getValue(const QString& name, google::protobuf::Message& data);
-
-      /**
-        * Remove a data.
-        * @return Return false if the data didn't exist.
-        */
       static bool rmValue(const QString& name);
    };
 }
