08bce3a3d61df333209ce2371e5ede32f6f9908f
Greg Burri <greg.burri@gmail.com>
2010-05-13 16:58:53 +0200

    Move the comment from .h to .cpp.

--- application/Core/FileManager/priv/FileUpdater/FileUpdater.h
+++ application/Core/FileManager/priv/FileUpdater/FileUpdater.h
@@ -29,12 +29,6 @@ namespace FM
 
       void stop();
 
-      /**
-        * Set the file cache to retrieve the hashes frome it.
-        * Muste be called before starting the fileUpdater.
-        * The shared dirs in fileCache must be previously added by 'addRoot(..)'.
-        * This object must unallocated the hashes.
-        */
       void setFileCache(const Protos::FileCache::Hashes* fileCache);
 
    public slots:
@@ -53,39 +47,16 @@ namespace FM
 
    private:
 
-      /**
-        * It will take some file from 'fileWithoutHashes' and compute theirs hashes.
-        * The duration of the compuation is minimum 'minimumDurationWhenHashing'.
-        * @return true if some file has been hashed
-        */
       bool computeSomeHashes();
 
-      /**
-        * Stop the current hashing process or the next hashing process.
-        */
       void stopHashing();
       /*void suspendHashing();
       void resumeHashing();*/
 
-      /**
-        * Synchronize the cache with the file system.
-        * Scan recursively all the directories and files contained
-        * in dir. Create the associated cached tree structure under
-        * given 'SharedDirectory'.
-        * Ths directories of files may already exist in the cache.
-        */
       void scan(Directory* dir);
 
-      /**
-        * If you omit 'dir' then all scanning will be removed
-        * from the queue.
-        */
       void stopScanning(Directory* dir = 0);
 
-      /**
-        * Try to restore the chunk hashes from 'fileCache'.
-        * 'fileCache' is set from 'retrieveFromFile(..)'.
-        */
       void restoreFromFileCache(SharedDirectory* dir);
 
       void treatEvents(const QList<WatcherEvent>& events);
