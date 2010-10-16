a968dac886e218fbf9771f47762f4a1822555eb0
Greg Burri <greg.burri@gmail.com>
2010-10-17 01:53:57 +0200

    * Add functions to set and get value string between QString and google::protobuf::Message. * Spelling

---
+++ application/Common/ProtoHelper.h
@@ -0,0 +1,35 @@
+#ifndef COMMON_PROTOHELPER_H
+#define COMMON_PROTOHELPER_H
+
+#include <QString>
+
+namespace Common
+{
+   class ProtoHelper
+   {
+   public:
+      template <typename T>
+      static void setStr(T& mess, void (T::*setter)(const char*), const QString& str);
+
+      template <typename T>
+      static QString getStr(const T& mess, const std::string& (T::*getter)() const);
+   };
+}
+
+/***** Definitions *****/
+using namespace Common;
+
+template <typename T>
+void ProtoHelper::setStr(T& mess, void (T::*setter)(const char*), const QString& str)
+{
+   QByteArray array = str.toUtf8();
+   (mess.*setter)(array.data());
+}
+
+template <typename T>
+QString ProtoHelper::getStr(const T& mess, const std::string& (T::*getter)() const)
+{
+   return QString::fromUtf8((mess.*getter)().data());
+}
+
+#endif
