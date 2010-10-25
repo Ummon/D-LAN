7ea56676caac76f3658e661e1e8c7c599219f909
Greg Burri <greg.burri@gmail.com>
2010-10-25 02:18:03 +0200

    Adding some mutex to the chunks index... not sure if it is useful..

--- application/Core/FileManager/priv/ChunkIndex/Chunks.cpp
+++ application/Core/FileManager/priv/ChunkIndex/Chunks.cpp
@@ -6,10 +6,29 @@ using namespace FM;
 
 void Chunks::add(QSharedPointer<Chunk> chunk)
 {
+   QMutexLocker lock(&this->mutex);
    this->insert(chunk->getHash(), chunk);
 }
 
 void Chunks::rm(QSharedPointer<Chunk> chunk)
 {
+   QMutexLocker lock(&this->mutex);
    this->remove(chunk->getHash());
 }
+
+const QSharedPointer<Chunk> Chunks::value(const Common::Hash& hash) const
+{
+   QMutexLocker lock(&this->mutex);
+   return QHash< Common::Hash, QSharedPointer<Chunk> >::value(hash);
+}
+
+bool Chunks::contains(const Common::Hash& hash) const
+{
+   QMutexLocker lock(&this->mutex);
+   return QHash< Common::Hash, QSharedPointer<Chunk> >::contains(hash);
+}
+
+/*const QSharedPointer<Chunk> operator[] (const Common::Hash& key) const
+{
+   return QHash::operator[](key);
+}*/
