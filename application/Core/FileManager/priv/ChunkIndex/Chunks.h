0de3c25b612666c70087f74f01aa93377555a480
Greg Burri <greg.burri@gmail.com>
2010-10-25 02:18:03 +0200

    Adding some mutex to the chunks index... not sure if it is useful..

--- application/Core/FileManager/priv/ChunkIndex/Chunks.h
+++ application/Core/FileManager/priv/ChunkIndex/Chunks.h
@@ -3,6 +3,7 @@
 
 #include <QHash>
 #include <QSharedPointer>
+#include <QMutex>
 
 #include <Common/Hash.h>
 
@@ -10,11 +11,17 @@ namespace FM
 {
    class Chunk;
 
-   class Chunks : public QHash< Common::Hash, QSharedPointer<Chunk> >
+   class Chunks : private QHash< Common::Hash, QSharedPointer<Chunk> >
    {
    public:
       void add(QSharedPointer<Chunk> chunk);
       void rm(QSharedPointer<Chunk> chunk);
+      const QSharedPointer<Chunk> value(const Common::Hash& hash) const;
+      bool contains(const Common::Hash& hash) const;
+      //const QSharedPointer<Chunk> operator[] (const Common::Hash& key) const;
+
+   private:
+      mutable QMutex mutex; // From the documentation : "they (containers) are thread-safe in situations where they are used as read-only containers by all threads used to access them.".
    };
 }
 #endif
