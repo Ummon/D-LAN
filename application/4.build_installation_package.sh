aae5ca0dffbc652e9c3c05d17b5069ff9a6f2f57
PowerKiKi <adrien.crivelli@gmail.com>
2010-11-27 01:13:40 +0100

    FIX display a message for installation packaging and do nothing instead of failure

--- application/4.build_installation_package.sh
+++ application/4.build_installation_package.sh
@@ -2,5 +2,11 @@
 
 # Makes the installation package, depending the platform it can be an .exe, deb, rpm, etc..
 
-# MS.Windows. Inno Setup (isetup) + QuickStart Pack (ispack) must be both installed. The Inno Setup directory must be placed in the PATH variable environnment.
-iscc aybabtu.iss
+
+if [ `uname -s` = "Linux" ] ; then
+	echo "No installation packaging for linux yet. Nothing to do."
+else
+    # MS.Windows. Inno Setup (isetup) + QuickStart Pack (ispack) must be both installed. The Inno Setup directory must be placed in the PATH variable environnment.
+    iscc aybabtu.iss
+fi
+
