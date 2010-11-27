370836e53d1aea82dcb0583d25c8bf878491605a
PowerKiKi <adrien.crivelli@gmail.com>
2010-11-27 01:01:31 +0100

    FIX testing script also support Linux

--- application/3.run_all_tests.sh
+++ application/3.run_all_tests.sh
@@ -2,9 +2,16 @@
 
 # Runs all tests. They must exist.
 
+if [ `uname -s` = "Linux" ] ; then
+	EXTENSION=
+else
+	EXTENSION=.exe
+fi
+
+
 # Common.
 cd Common/tests/output/release
-./Tests.exe
+./Tests$EXTENSION
 ERR=$?
 cd ../../../..
 if (( $ERR )); then exit 1; fi
