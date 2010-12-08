ee9da743b8da767bdf9b5e6ccc2d6523df2e4867
Greg Burri <greg.burri@gmail.com>
2010-12-08 22:17:05 +0100

    * Add a test for the received hashes.

--- application/Core/FileManager/TestsFileManager/HashesReceiver.cpp
+++ application/Core/FileManager/TestsFileManager/HashesReceiver.cpp
@@ -1,6 +1,8 @@
 #include <HashesReceiver.h>
 
 #include <QtDebug>
+#include <QElapsedTimer>
+#include <QTest>
 
 /**
   * @class HashesReceiver
@@ -13,9 +15,45 @@ HashesReceiver::HashesReceiver()
 {
 }
 
+/**
+  * @return false if timeouted or if a hash is incorrect.
+  * @param timeout [ms]
+  */
+bool HashesReceiver::waitToReceive(QList<Common::Hash>& hashes, int timeout)
+{
+   QListIterator<Common::Hash> i(this->receivedHashes);
+   QListIterator<Common::Hash> j(hashes);
+
+
+   QElapsedTimer timer;
+   timer.start();
+
+   forever
+   {
+      while (i.hasNext() && j.hasNext())
+      {
+         Common::Hash h1 = i.next();
+         Common::Hash h2 = j.next();
+         if (h1 != h2)
+         {
+            qDebug() << "Error : " << h1.toStr() << " != " << h2.toStr();
+            return false;
+         }
+      }
+      if (this->receivedHashes.size() == hashes.size())
+         return true;
+
+      QTest::qWait(50);
+
+      if (timer.elapsed() > timeout)
+         return false;
+   }
+}
+
 void HashesReceiver::nextHash(Common::Hash hash)
 {
    qDebug() << this->num << " : " << hash.toStr();
+   this->receivedHashes << hash;
    this->num++;
 }
 
