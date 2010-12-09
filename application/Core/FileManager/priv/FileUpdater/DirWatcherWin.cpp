d1d3b4f971ece573984e4f1114aab82276b5df81
Greg Burri <greg.burri@gmail.com>
2010-12-09 01:13:45 +0100

    You saw nothing.

--- application/Core/FileManager/priv/FileUpdater/DirWatcherWin.cpp
+++ application/Core/FileManager/priv/FileUpdater/DirWatcherWin.cpp
@@ -95,7 +95,7 @@ const QList<WatcherEvent> DirWatcherWin::waitEvent(int timeout, QList<WaitCondit
    {
       L_ERRO(QString("DirWatcherWin::waitEvent : No more than %1 condition(s), some directory will not be watched any more.").arg(MAX_WAIT_CONDITION));
       int n = this->dirs.size() + ws.size() - MAXIMUM_WAIT_OBJECTS;
-      while (n --> 0) // The better C++ operator!
+      while (n --> 0) // The best C++ operator!
          this->dirsToDelete << this->dirs.takeLast();
    }
 
