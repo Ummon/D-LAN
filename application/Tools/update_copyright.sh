20227c26becb6fc2083b3f29b3f90f887c83c69a
Greg Burri <greg.burri@gmail.com>
2010-12-17 03:53:28 +0100

    Add update tool for copyright and license header.

--- application/Tools/update_copyright.sh
+++ application/Tools/update_copyright.sh
@@ -3,17 +3,38 @@
 # This script will integrate the license '../license.txt' to all source (*.h, *.cpp)
 # If the license already exists in a source it will be replaced.
 
-# TODO
+set -o errexit
 
 cd ..
 
-for i in *.cpp
+LICENSE_FILENAME="license.txt"
+LICENSE_SIZE=`wc -l $LICENSE_FILENAME | awk '{print $1}'`
+LICENSE=`cat "$LICENSE_FILENAME"`
+
+echo -e "Current copyright and license header :\n$LICENSE"
+echo
+
+NB_FILE_UPDATED=0
+
+for CURRENT_FILE in `find . -not -path "./Libs*" -and -not -path "*/.tmp*" -and \( -name '*.cpp' -or -name '*.h' \)`
 do
-   echo $i
-  #if ! grep -q Copyright $i
-  #then
-  #  cat copyright.txt $i >$i.new && mv $i.new $i
-  #fi
+   echo "Current File : $CURRENT_FILE"
+   TMP_FILE="${CURRENT_FILE}.tmp"
+   if [ "$LICENSE" != "`head -n $LICENSE_SIZE $CURRENT_FILE`" ]
+   then
+      NB_FILE_UPDATED=`expr $NB_FILE_UPDATED + 1`
+      if [ "`sed -n '5,8p' $LICENSE_FILENAME`" = "`sed -n '5,8p' \"$CURRENT_FILE\"`" ]
+      then
+         echo "$LICENSE" > "$TMP_FILE"
+         sed "1,${LICENSE_SIZE}d" "$CURRENT_FILE" >> "$TMP_FILE"
+         mv "$TMP_FILE" "$CURRENT_FILE"
+      else
+         echo "$LICENSE" | cat - "$CURRENT_FILE" > "$TMP_FILE"
+         mv "$TMP_FILE" "$CURRENT_FILE"
+      fi
+   else
+      echo "Nothing to do"
+   fi
 done
 
-
+echo "Number of file updated : $NB_FILE_UPDATED"
