eb415eaa81ad63f741d6e2157af6e24a9b836a94
Hervé Martinet <herve.martinet@gmail.com>
2010-12-17 15:53:12 +0100

    Re-merge after master pull

--- application/Common/Version.h
+++ application/Common/Version.h
@@ -23,7 +23,7 @@
 #define VERSION_TAG "Alpha"
 
 // These two values are automatically updated during the release building process. See the script 'Application/Tools/update_version.sh'
-#define BUILD_TIME "2010-12-17_14-23"
-#define GIT_VERSION "a34af79bf886d2d7073c84b63d0cf009ca160781"
+#define BUILD_TIME "2010-12-17_14-49"
+#define GIT_VERSION "f6ae28c246b372b0c3239d80a7189b8fd06aeeaf"
 
 #endif
