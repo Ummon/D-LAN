d507aeacb29f8148385b106aa5c139d9ef7d7f66
Greg Burri <greg.burri@gmail.com>
2010-12-17 03:36:20 +0100

    Add copyright and license at the beginning of each source file.

--- application/Common/Version.h
+++ application/Common/Version.h
@@ -1,3 +1,21 @@
+/**
+  * Aybabtu - A decentralized LAN file sharing software.
+  * Copyright (C) 2010-2011 Greg Burri <greg.burri@gmail.com>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+  
 #ifndef COMMON_VERSION_H
 #define COMMON_VERSION_H
 
@@ -5,7 +23,7 @@
 #define VERSION_TAG "Alpha"
 
 // These two values are automatically updated during the release building process. See the script 'Application/Tools/update_version.sh'
-#define BUILD_TIME "2010-11-15_00-16"
-#define GIT_VERSION "64db790ed2ff97991f547541bd48007c2ea2afa5"
+#define BUILD_TIME "2010-12-16_01-01"
+#define GIT_VERSION "56f15c16eb0631456392c7b65850414244bfc9f4"
 
 #endif
