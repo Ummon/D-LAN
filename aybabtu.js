0128e1504ddc76410f64fbc5a8c340e86da012e5
Greg Burri <greg.burri@gmail.com>
2011-01-10 20:13:07 +0100

    * Add a flattr button * Remove the javascript email generation

--- aybabtu.js
+++ aybabtu.js
@@ -1,33 +1,6 @@
 // all aybabtu is contained in this object
 var aybabtu = {};
-
-aybabtu.rot13 = function(chaine) {
-   var ACode = 'A'.charCodeAt(0);
-   var aCode = 'a'.charCodeAt(0);
-   var MCode = 'M'.charCodeAt(0);
-   var mCode = 'm'.charCodeAt(0);
-   var ZCode = 'Z'.charCodeAt(0);
-   var zCode = 'z'.charCodeAt(0);
-
-   var f = function(ch, pos) {
-      if (pos == ch.length) {
-         return ""
-      }      
-      var c = ch.charCodeAt(pos);
-      return String.fromCharCode(
-         c +
-         (c >= ACode && c <= MCode || c >= aCode && c <= mCode ?  13 :
-         (c >  MCode && c <= ZCode || c >  mCode && c <= zCode ? -13 : 0))
-      ) + f(ch, pos + 1)
-   }
-   return f(chaine, 0);
-}
    
-$(document).ready(
-   function() { 
+$(function() { 
       $('.gallery a').colorbox();
-      var mailTo = aybabtu.rot13("znvygb:");
-      $("#emailLinkGB").attr("href", mailTo + aybabtu.rot13("tert.oheev@tznvy.pbz"));
-      $("#emailLinkHM").attr("href", mailTo + aybabtu.rot13("ureir.znegvarg@tznvy.pbz"));
-   }
-);
+   });
