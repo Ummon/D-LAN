bb59a98d1627b083bba0354869033054d90ca80b
Greg Burri <greg.burri@gmail.com>
2011-01-10 20:09:43 +0100

    Add a deploy script.

---
+++ tools/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+if (( $# < 1 ))
+then
+   echo "Usage : $0 <target>"
+   echo " <target> : a directory where the website will be deployed"
+   exit 1
+fi;
+
+TARGET=$1
+mkdir $TARGET
+
+if [ ! -d $TARGET ]
+then
+   echo "The target directory \"$TARGET\" doesn't exist"
+   exit 1
+fi;
+
+RSYNC_OPTS=-av
+
+cd ..
+
+mkdir $TARGET/db # Yaws should have the permission to write in this folder.
+
+rsync $RSYNC_OPTS colorbox $TARGET
+rsync $RSYNC_OPTS files $TARGET
+rsync $RSYNC_OPTS img $TARGET
+rsync $RSYNC_OPTS modules/ebin $TARGET/modules/
+rsync $RSYNC_OPTS modules/include $TARGET/modules/
+rsync $RSYNC_OPTS yssi $TARGET
+rsync $RSYNC_OPTS aybabtu.js $TARGET
+rsync $RSYNC_OPTS favicon.ico $TARGET
+rsync $RSYNC_OPTS index.yaws $TARGET
+rsync $RSYNC_OPTS jquery* $TARGET
+rsync $RSYNC_OPTS style.css $TARGET
+ 
