8130b1472cf0f9d686d45167c83828b49ad2051a
Greg Burri <greg.burri@gmail.com>
2011-01-13 21:54:18 +0100

    Split the core in two class : * Core. * CoreService : Add service features to Core.

--- application/Core/Core.cpp
+++ application/Core/Core.cpp
@@ -1,27 +1,5 @@
-/**
-  * Aybabtu - A decentralized LAN file sharing software.
-  * Copyright (C) 2010-2011 Greg Burri <greg.burri@gmail.com>
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
-  
 #include <Core.h>
 using namespace CoreSpace;
-
-#include <QObject>
-#include <QThread>
-#include <QRegExp>
 
 #include <Protos/core_settings.pb.h>
 
@@ -34,8 +12,7 @@ using namespace CoreSpace;
 #include <NetworkListener/Builder.h>
 #include <RemoteControlManager/Builder.h>
 
-Core::Core(int argc, char** argv)
-   : QtService<QCoreApplication>(argc, argv, Common::SERVICE_NAME)
+Core::Core()
 {
    GOOGLE_PROTOBUF_VERIFY_VERSION;
 
@@ -50,22 +27,6 @@ Core::Core(int argc, char** argv)
    catch(Common::PersistentDataIOException& err)
    {
       L_ERRO(err.message);
-   }
-
-   this->setServiceDescription("A LAN file sharing system");
-   this->setStartupType(QtServiceController::ManualStartup);
-   this->setServiceFlags(QtServiceBase::Default);
-
-   // If AybabtuCore is launched from the console we read user input.
-   for (int i = 1; i < argc; i++)
-   {
-      QString currentArg = QString::fromAscii(argv[i]);
-      if (currentArg == "-e" || currentArg == "-exec")
-      {
-         connect(&this->consoleReader, SIGNAL(newLine(QString)), this, SLOT(treatUserInput(QString)), Qt::QueuedConnection);
-         this->consoleReader.start();
-         break;
-      }
    }
 }
 
@@ -86,27 +47,6 @@ void Core::start()
 
    L_USER("Ready to serve");
 }
-
-void Core::stop()
-{
-   this->application()->quit();
-   this->consoleReader.stop();
-}
-
-void Core::treatUserInput(QString input)
-{
-   if (input == ConsoleReader::QUIT_COMMAND)
-   {
-      this->stop();
-   }
-   else
-   {
-      QTextStream out(stdout);
-      out << "Commands:" << endl
-          << " - " << ConsoleReader::QUIT_COMMAND << " : stop the core" << endl;
-   }
-}
-
 
 /**
   * Check if each value settings is valid, for example buffer_size cannot be one byte or 3 TiB..
@@ -162,7 +102,3 @@ void Core::checkSettingsIntegrity()
    this->checkSetting("remote_max_nb_connection", 1u, 1000u);
    this->checkSetting("search_lifetime", 1000u, 60 * 1000u);
 }
-
-
-
-
