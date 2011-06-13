ab241ce0fc7f6b12606cc093041fc2e058505a48
Greg Burri <greg.burri@gmail.com>
2011-06-13 22:24:26 +0200

    Some clean-up.

--- modules/erl/d_lan_common.erl
+++ modules/erl/d_lan_common.erl
@@ -1,22 +1,32 @@
 -module(d_lan_common).
 -export([current_page/1, page_name/2, menu/1, image/2, images/1, download_button/2, send_release/3]).
+
 -import(d_lan_lang, [tr/3, tr/4]).
+-import(lists, [member/2, map/2, last/1, sort/1]).
  
 -include("/usr/lib/yaws/include/yaws_api.hrl"). 
 -include("../include/d_lan_defines.hrl").
+
+-type page() :: atom().
+-spec current_page(#arg{}) -> page() | unknown.
+-spec page_name(#arg{}, page()) -> string().
+-spec menu(#arg{}) -> {ehtml, term()}.
+-spec image(string(), string()) -> {'div', term()}.
+-spec download_button(#arg{}, string()) -> {'div', term()}.
+-spec send_release(#arg{}, string(), string()) -> [#headers{}] | error.
  
 pages() ->
    [home, features, faq, about].
-   
+
 hidden_pages() ->
    [stats, donate].
    
-% Return the current page depending the page parameters -> atom()
+% Return the current page depending the page parameters.
 current_page(A) ->
    case yaws_api:queryvar(A, "p") of
       {ok, PageStr} ->
          Page = list_to_atom(PageStr),
-         case lists:member(Page, pages() ++ hidden_pages()) of
+         case member(Page, pages() ++ hidden_pages()) of
             true -> Page;
             _ -> unknown
          end;
@@ -28,7 +38,7 @@ page_name(A, P) -> tr(menu, P, A).
 menu(A) ->
    Current_page = current_page(A),
    {ehtml,
-      lists:map(
+      map(
          fun(E) ->
             {li, [], [
                {a, [{href, atom_to_list(E) ++ ".html"}] ++ if Current_page =:= E -> [{class, "current-page"}]; true -> [] end, page_name(A, E)}]}
@@ -48,7 +58,7 @@ image(Filename, Caption) ->
    }.
    
 images(Filename_caption_list) ->
-   lists:map(
+   map(
       fun({Filename, Caption}) -> 
          image(Filename, Caption)
       end,
@@ -61,7 +71,7 @@ download_button(A, Platform) ->
    Platform_maj = [string:to_upper(hd(Platform)) | tl(Platform)],
    Relase_platform_folder = A#arg.docroot ++ "/" ++ ?RELEASES_FOLDER ++ "/" ++ Platform,
    {ok, Filenames} = file:list_dir(Relase_platform_folder),
-   Filename = lists:last(lists:sort(lists:filter(fun(F) -> string:right(F, 4) =:= ".exe" end, Filenames))),
+   Filename = last(sort([F || F <- Filenames, string:right(F, 4) =:= ".exe"])),
    {match, [Version, Version_tag, Year, Month, Day]} = re:run(Filename, "D-LAN-((?:\\d|\\.)+)([^-]*)-(\\d+)-(\\d+)-(\\d+).*", [{capture, all_but_first, list}]),
    File_size = filelib:file_size(Relase_platform_folder ++ "/" ++ Filename),
    File_size_mb = float(File_size) / 1048576,
@@ -86,7 +96,7 @@ send_release(A, Filename, Platform) ->
          send_stream(Filename, IoDevice),
          % See : http://en.wikipedia.org/wiki/List_of_HTTP_header_fields
          [{header, {content_length, File_size}}, {header, "Content-Disposition: attachment; filename=" ++ Filename}, {streamcontent, "application/octet-stream", <<>>}];
-      _ -> ok
+      _ -> error
    end.
 
 send_stream(Filename, IoDevice) ->
