c6cdfce6f67bb5314c717fd84310424013dc48b5
Greg Burri <greg.burri@gmail.com>
2012-05-19 22:03:20 +0200

    Add a missing copyright header.

--- application/Client/D-LAN_Client.cpp
+++ application/Client/D-LAN_Client.cpp
@@ -15,7 +15,7 @@
   * You should have received a copy of the GNU General Public License
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
-
+  
 #include <QFile>
   
 #include <D-LAN_Client.h>
