bd807b2e832617f6ac83a225195fa65b5f6e9a2d
Greg Burri <greg.burri@gmail.com>
2012-07-15 11:44:59 +0200

    * Use the word 'Folder' instead of 'Directory' for the user interface. * The skin images wasn't found on Linux.

--- application/GUI/Settings/DirListModel.cpp
+++ application/GUI/Settings/DirListModel.cpp
@@ -123,7 +123,7 @@ const QList<Common::SharedDir>& DirListModel::getDirs() const
 Common::SharedDir DirListModel::getDir(const Common::Hash& ID) const
 {
    for (QListIterator<Common::SharedDir> i(this->dirs); i.hasNext();)
-   {
+   {directory
       Common::SharedDir dir = i.next();
       if (dir.ID == ID)
          return dir;
@@ -178,7 +178,7 @@ QVariant DirListModel::headerData(int section, Qt::Orientation orientation, int 
    case Qt::DisplayRole:
       switch (section)
       {
-      case 0: return tr("Directory");
+      case 0: return tr("Folder");
       case 1: return tr("Size");
       case 2: return tr("Free space");
       default: return QAbstractTableModel::headerData(section, orientation, role);
