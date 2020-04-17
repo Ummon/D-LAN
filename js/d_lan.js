5e5d57577eef8a7462a4f7c4e9d05d7e79709f9f
Ummon <greg.burri@gmail.com>
2020-04-17 22:53:11 +0200

    Update jQuery and colorbox

--- js/d_lan.js
+++ js/d_lan.js
@@ -4,23 +4,23 @@ var dlan = {};
 dlan.isMobile = function() {
     return (/webos|iphone|ipod|android|ie|blackberry|windows phone/).test(navigator.userAgent.toLowerCase());
 }
-   
+
 $(function() {
    $(".gallery a").colorbox();
-   
+
    var canvas = $("#canvas-menu")[0];
-   canvas.height = 125; 
+   canvas.height = 125;
    var setCanvasSize = function() { canvas.width = window.innerWidth; };
-   $(window).resize(setCanvasSize);   
+   $(window).resize(setCanvasSize);
    setCanvasSize();
-   
+
    // It snows from the begining of december to the end of february.
    var currentMonth = (new Date()).getMonth();
    if (currentMonth >= 11 || currentMonth <= 0 && !dlan.isMobile())
    {
       var snow = new Snow(canvas);
-      
-      /*      
+
+      /*
       snow.p.flakeSpeedFactor = 4;
       snow.p.flakeSizeFactor = 5;
       snow.p.blur = 0.5;
@@ -28,9 +28,9 @@ $(function() {
       snow.p.transparencyFactorFromPosition = function(y, r) { return 1; }
       snow.p.flakeColorFromPosition = function(x, y) {
          return { r: Math.floor(x * 255) % 255, g: 210, b: 255 - Math.floor(y * 255 * 4) % 255 };
-      } 
+      }
       */
-      
+
       snow.start();
    }
 });
