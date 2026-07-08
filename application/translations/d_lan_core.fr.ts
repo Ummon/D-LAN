6313c83c9b293a7359efe3cf19985e0e28139c2e
Greg Burri <greg.burri@gmail.com>
2026-07-08 20:26:25 +0200

    Update french translations

--- application/translations/d_lan_core.fr.ts
+++ application/translations/d_lan_core.fr.ts
@@ -4,34 +4,21 @@
 <context>
     <name>CoreSpace::CoreService</name>
     <message>
+        <location filename="../Core/CoreService.cpp" line="33"/>
         <source>A LAN file sharing system</source>
         <translation>Un système de partage de fichier sur LAN</translation>
     </message>
 </context>
 <context>
-    <name>DM::ChunkDownload</name>
-    <message>
-        <source>Corrupted data received for the file &quot;%1&quot; from peer %2. Peer banned for %3 ms</source>
-        <translation type="obsolete">Données corrompues reçues dans le fichier &quot;%1&quot; du pair %2. Le pair est banni durant %3 ms</translation>
-    </message>
-    <message>
-        <source>Has sent corrupted data</source>
-        <extracomment>A reason why the user has been banned</extracomment>
-        <translation type="obsolete">A envoyé des données corrompues</translation>
-    </message>
-</context>
-<context>
     <name>DM::ChunkDownloader</name>
     <message>
-        <source>Corrupted data received for the file &quot;%1&quot; from peer %2. Peer banned for %3 ms</source>
-        <translation type="obsolete">Données corrompues reçues pour le fichier &quot;%1&quot; du pair %2. Le pair est banni durant %3 ms</translation>
-    </message>
-    <message>
+        <location filename="../Core/DownloadManager/priv/ChunkDownloader.cpp" line="295"/>
         <source>Has sent corrupted data</source>
         <extracomment>A reason why the user has been blocked</extracomment>
         <translation>A envoyé des données corrompues</translation>
     </message>
     <message>
+        <location filename="../Core/DownloadManager/priv/ChunkDownloader.cpp" line="290"/>
         <source>Corrupted data received for the file &quot;%1&quot; from peer %2. Peer blocked for %3 ms</source>
         <translation>Données corrompues reçues pour le fichier &quot;%1&quot; du pair %2. Le pair est blocké durant %3 ms</translation>
     </message>
@@ -39,6 +26,7 @@
 <context>
     <name>DM::DownloadManager</name>
     <message>
+        <location filename="../Core/DownloadManager/priv/DownloadManager.cpp" line="205"/>
         <source>The file &apos;%1&apos; is already in queue</source>
         <translation>Le fichier &apos;%1&apos; est déjà en queue</translation>
     </message>
@@ -46,6 +34,7 @@
 <context>
     <name>DM::FileDownload</name>
     <message>
+        <location filename="../Core/DownloadManager/priv/FileDownload.cpp" line="458"/>
         <source>File completed: %1%2%3</source>
         <translation>Fichier complet: %1%2%3</translation>
     </message>
@@ -53,24 +42,15 @@
 <context>
     <name>FM::FileHasher</name>
     <message>
-        <source>Computing the hashes for %1 . . .</source>
-        <translation type="vanished">Calcul des empreintes pour %1...</translation>
-    </message>
-    <message>
+        <location filename="../Core/FileManager/priv/Cache/FileHasher.cpp" line="87"/>
         <source>Computing hashes of %1 . . .</source>
         <translation>Calcul des empreintes pour %1...</translation>
     </message>
 </context>
 <context>
-    <name>PM::PeerManager</name>
-    <message>
-        <source>Our current ID: %1</source>
-        <translation type="obsolete">Votre ID actuel: %1</translation>
-    </message>
-</context>
-<context>
     <name>PM::PeerSelf</name>
     <message>
+        <location filename="../Core/PeerManager/priv/PeerSelf.cpp" line="38"/>
         <source>Our current ID: %1</source>
         <translation>Votre ID actuel: %1</translation>
     </message>
@@ -78,22 +58,22 @@
 <context>
     <name>QObject</name>
     <message>
-        <source>Starting..</source>
-        <translation type="obsolete">Démarrage..</translation>
-    </message>
-    <message>
+        <location filename="../Core/Core.cpp" line="123"/>
         <source>Ready to serve</source>
         <translation>Prêt à servir</translation>
     </message>
     <message>
+        <location filename="../Core/DownloadManager/priv/DownloadQueue.cpp" line="342"/>
         <source>The version (%1) of the queue file &quot;%2&quot; doesn&apos;t match the current version (%3). Queue will be reset.</source>
         <translation>La version (%1) du fichier contenant la queue &quot;%2&quot; ne correspond pas à la version courante (%3). La queue va être réinitialisée.</translation>
     </message>
     <message>
+        <location filename="../Core/Core.h" line="78"/>
         <source>Shutdown</source>
         <translation>Arrêté</translation>
     </message>
     <message>
+        <location filename="../Core/Core.cpp" line="105"/>
         <source>D-LAN Core version %1 is starting . . .</source>
         <translation>D-LAN Core version %1 démarre...</translation>
     </message>
