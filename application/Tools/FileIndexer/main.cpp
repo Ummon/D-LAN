414a738790cc499c9bde97c95571cac9283f5941
Greg Burri <greg.burri@gmail.com>
2026-08-16 02:48:38 +0200

    Replace QLinkedList by QList

--- application/Tools/FileIndexer/main.cpp
+++ application/Tools/FileIndexer/main.cpp
@@ -18,7 +18,7 @@
   
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDir>
-#include <QtCore/QLinkedList>
+#include <QtCore/QList>
 #include <QtCore/QElapsedTimer>
 #include <QtCore/QTextStream>
 #include <QtCore/QPair>
@@ -45,7 +45,7 @@ QTextStream out(stdout);
 template <typename T>
 void add(WordIndex<T>& index, const QString& word, const T& item)
 {
-   out << "Add item : " << item << " indexed by " << word << endl;
+   out << "Add item : " << item << " indexed by " << word << Qt::endl;
    index.addItem(word, item);
 }
 
@@ -65,16 +65,16 @@ void search(WordIndex<T>& index, const QString& word, int n)
 
    qint64 time = t.elapsed();
 
-   out << "Search \"" << word << "\" : " << items.count() << " items found" << endl;
+   out << "Search \"" << word << "\" : " << items.count() << " items found" << Qt::endl;
    foreach (T item, items)
-      out << " - " << item << endl;
-   out << " Time: " << double(time) / 1000 << " s" << endl;
+      out << " - " << item << Qt::endl;
+   out << " Time: " << double(time) / 1000 << " s" << Qt::endl;
 }
 
 template <typename T>
 void rm(WordIndex<T>& index, const QString& word, const T& item)
 {
-   out << "Remove " << item << " with word " << word << endl;
+   out << "Remove " << item << " with word " << word << Qt::endl;
    QList<QString> words;
    words.append(word);
    index.rmItem(words, item);
@@ -86,7 +86,7 @@ void rm(WordIndex<T>& index, const QString& word, const T& item)
 template <typename T>
 void indexFile(WordIndex<T>& index, const QString& fileName, const T& item)
 {
-   const QStringList& words = Common::Global::splitInWords(fileName);
+   const QStringList& words = Common::StringUtils::splitInWords(fileName);
 
    index.addItem(words, item);
 }
@@ -109,11 +109,11 @@ QString buildItem<QString>(const QFileInfo& entry)
 template <typename T>
 void buildIndex(WordIndex<T>& index, const QString& path)
 {
-   QLinkedList< QPair< QString, T > > items;
+   QList<QPair<QString, T> > items;
 
-   out << "Scanning..." << endl;
+   out << "Scanning..." << Qt::endl;
 
-   QLinkedList<QDir> dirsToVisit;
+   QList<QDir> dirsToVisit;
    dirsToVisit.append(path);
    while (!dirsToVisit.isEmpty())
    {
@@ -129,7 +129,7 @@ void buildIndex(WordIndex<T>& index, const QString& path)
       }
    }
 
-   out << "Indexing..." << endl;
+   out << "Indexing..." << Qt::endl;
    QElapsedTimer t;
    t.start();
 
@@ -140,14 +140,14 @@ void buildIndex(WordIndex<T>& index, const QString& path)
       indexFile(index, i->first, i->second);
    }
 
-   out << n << " items indexed in " << double(t.elapsed()) / 1000 << " s" << endl;
+   out << n << " items indexed in " << double(t.elapsed()) / 1000 << " s" << Qt::endl;
 }
 
 void printUsage(int argc, char *argv[])
 {
    QTextStream out(stdout);
-   out << "Usage : " << argv[0] << " <directory>*" << endl
-      << " <directory> : will scan recursively the directory and index each file and folder." << endl;
+   out << "Usage : " << argv[0] << " <directory>*" << Qt::endl
+      << " <directory> : will scan recursively the directory and index each file and folder." << Qt::endl;
 }
 
 int main(int argc, char *argv[])
